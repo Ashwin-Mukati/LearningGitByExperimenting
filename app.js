80b864f3ed3d1d5812930dc98c1135d04fc3f567
Ashwin-Mukati <ashwinmukati40@gmail.com>
2020-01-04 16:13:32 +0530

    devsena

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ let whateverinTutorialjs = require('./tutorial');
 console.log(whateverinTutorialjs);
 console.log(whateverinTutorialjs.sum(1,2));
 console.log(new whateverinTutorialjs.MyClass());
-// let newObj= new whateverinTutorialjs.MyClass();
+//             
